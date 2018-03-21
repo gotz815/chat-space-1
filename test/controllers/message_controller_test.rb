2f034c02f6e6f4a37b036a00a04ac21c857e05b9
Maeda Kanako <knk.itg.milk@gmail.com>
2018-03-21 15:03:30 +0900

    edit-makup-view

--- test/controllers/message_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MessageControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get message_index_url
-    assert_response :success
-  end
-
-end
