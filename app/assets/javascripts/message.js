84ac6509cd8670f1e3b3b932d6308ca89b98aa2c
Maeda Kanako <knk.itg.milk@gmail.com>
2018-04-01 10:54:20 +0900

    first commit
    jsファイル作成、編集
    HTML,JSON処理分け
    jbuilder使用、JSON形式で返す
    HTML作成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,36 @@
-$(function() {
-	$('#new_message').on('submit', function(e) {
-		e.preventdefault();
-		console.log(this)
-		var formData = new formData(this);
-	})
+$(function(){
+  function buildHTML(message){
+    var image_url=(message.image_url !=null)? `<image class="lower-message_image" src="${message.imgae_url}">`:"";
+    var html = `<div class="upper-message">
+                  <div class="upper-message__name">${message.name}></div>
+                  <div class="upper-message__time">${message.time}></div>
+                  <p class="lower-message__content">${message.content}</p>
+                  ${image_url}
+                </div>`
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html); 
+      $('.form__message').val('');
+      $('.form__submit').attr("disabled",false);
+      $(".message").animate({scrollTop: $(".message")[0].scrollHeight}, 'fast');
+    })
+    .fail(function(data){
+      alert('error');
+      $(".form__submit").attr("disabled",false);
+    })
+  })
 })
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,5 @@
+json.content @message.content
+json.name @message.user.name
+json.time @message.created_at
+json.imgae @message.image
+json.image_url @message.image_url
