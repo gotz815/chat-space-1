022adcf830f839dce55123536e3d7f823ae42e10
Maeda Kanako <knk.itg.milk@gmail.com>
2018-04-12 20:13:34 +0900

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
-set :ssh_options, auth_methods: [54.95.54.206],
+set :ssh_options, auth_methods: ['54.95.54.206'],
                   keys: ['/Users/kanako/.ssh/knkrnrn.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
