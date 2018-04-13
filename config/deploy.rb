f9e8ab0d84875c9e356df8a3afb1e7c08ad93b3a
Maeda Kanako <knk.itg.milk@gmail.com>
2018-04-13 15:35:48 +0900

    setting-AWS-S3

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :application, "chat-space"
 set :repo_url, "git@github.com:itgmilk/chat-space.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, %w{ config/secrets.yml }
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
@@ -15,6 +16,7 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
+
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
